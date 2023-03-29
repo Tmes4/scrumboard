03a73909345e98650c16a4b9b991e7935fca85d7
Abbas <abas_235@hotmail.com>
2023-03-29 20:09:52 +0200

    magrib tijd header nog steeds niet af

--- header.php
+++ header.php
@@ -1,15 +1,32 @@
 <?php require_once 'backend/config.php' ?>
 
 <header>
-    <div class="navigation">
-        <nav>
-            <img src="" alt="">
-            <a href="<?php echo $base_url; ?>./index.php">Home</a>
-            <a href="<?php echo $base_url; ?>./task/create.php">Create</a> <!-- dit is alleen om de pagina te testen straks kan deze weg en je eigen ding er in zetten  -->
-        </nav>
-
-        <div>
-            <a href="<?php echo $base_url; ?>./login.php">Inloggen</a>
+    <div class="header-area">
+        <div class="logo">
+            <img src="./img/logo-big-v4.png" alt="">
+            <h2>Developer Land</h2>
         </div>
+
+        <div class="navbar-area">
+            <div class="nav-holder">
+                <ul>
+                    <li><a href="<?php echo $base_url; ?>./index.php">Home</a></li>
+                    <li><a href="<?php echo $base_url; ?>./task/index.php">Scrumboard</a></li>
+                    <li><a href="<?php echo $base_url; ?>./setting/setting.php">Instelling</a></li>
+                    <li><a href="<?php echo $base_url; ?>./login.php">Inloggen</a></li>
+                </ul>
+            </div>
+            <div class="profile">
+                <img src="./img/img_avatar.png" alt="">
+                <p>EIGEN NAAM</p>
+            </div>
+
+
+        </div>
+
+
+
+
+
     </div>
-</header>   
+</header>
