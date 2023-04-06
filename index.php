d848b806f7fde56a84515861c8931dd55a93e30d
Ismail Ameksa <117262493+DRismaill@users.noreply.github.com>
2023-04-06 11:06:39 +0200

    Update index.php
    ? dit was van gister met abbas in de avond

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link rel="stylesheet" href="<?php echo $base_url; ?>./css/normalize.css">
     <link rel="stylesheet" href="<?php echo $base_url; ?>./css/main.css"> -->
-    <link rel="stylesheet" href="/css/home.css">
-    <!-- <?php echo $base_url; ?>. -->
+    <link rel="stylesheet" href="<?php echo $base_url; ?>./css/home.css">
+
 
     <script src="https://kit.fontawesome.com/06a9306cce.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
